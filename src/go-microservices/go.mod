bbbd7736f7b3409df416c45dd0fad23ac01b2618
putrasinaga <putrasaut@gmail.com>
2022-12-27 19:20:14 +0700

    add all

---
+++ src/go-microservices/go.mod
@@ -0,0 +1,5 @@
+module github.com/PutraSinaga/go-web-app-microservices.git
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
---
+++ src/go-microservices/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "log"
+    "github.com/gorilla/mux"
+    "time"
+    "encoding/json"
+)
+
+
+func healthHandler(w http.ResponseWriter, r *http.Request){
+    log.Println("checking health")
+    response := map[string]string{
+        "Status" : "ok", 
+        "time" : time.Now().String(), 
+    }
+
+    json.NewEncoder(w).Encode(response)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request){
+ 
+    fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    w.WriteHeader(http.StatusOK)
+}
+
+func detailHandler(w http.ResponseWriter, r *http.Request){
+
+    fmt.Println("lihat detail..")
+
+    hostname, err := detail.GetHostname
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(hostname)
+ 
+}
+
+
+func main() {
+    r := mux.NewRouter()
+    r.HandleFunc("/health", healthHandler)
+    r.HandleFunc("/", rootHandler)
+
+
+    log.Fatal(http.ListenAndServe(":8000", r))
+}
